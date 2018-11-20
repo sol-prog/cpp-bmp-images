de494066b2630bd299946a6695dea6a25da44272
sol-prog <solarianprogrammer@gmail.com>
2018-11-20 16:21:15 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cpp-bmp-images
+Source files for [C++ reading and writing BMP images](https://solarianprogrammer.com/2018/11/19/cpp-reading-writing-bmp-images/)
---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "BMP.h"
+
+int main() {
+	BMP bmp9("t1_24.bmp");
+	bmp9.fill_region(0, 0, 50, 50, 0, 0, 255, 255);
+	bmp9.fill_region(150, 0, 100, 150, 0, 255, 0, 255);
+	bmp9.write("t1_24_copy.bmp");
+
+	BMP bmp10("t2_24.bmp");
+	bmp10.fill_region(0, 0, 50, 50, 0, 0, 255, 255);
+	bmp10.fill_region(150, 0, 100, 150, 0, 255, 0, 255);
+	bmp10.write("t2_24_copy.bmp");
+
+	BMP bmp5("Shapes_24.bmp");
+	bmp5.fill_region(0, 0, 100, 200, 0, 0, 255, 255);
+	bmp5.fill_region(150, 0, 209, 203, 0, 255, 0, 255);
+	bmp5.write("Shapes_24_copy.bmp");
+
+	// Read an image from disk and write it back:
+	BMP bmp("Shapes.bmp");
+	bmp.fill_region(0, 0, 100, 200, 0, 0, 255, 255);
+	bmp.write("Shapes_copy.bmp");
+
+	// Create a BMP image in memory, modify it, save it on disk
+	BMP bmp2(800, 600);
+	bmp2.fill_region(50, 20, 100, 200, 0, 0, 255, 255);
+	bmp2.write("img_test.bmp");
+
+	// Create a 24 bits/pixel BMP image in memory, modify it, save it on disk
+	BMP bmp3(200, 200, false);
+	bmp3.fill_region(50, 20, 100, 100, 255, 0, 255, 255);
+	bmp3.write("img_test_24bits.bmp");
+
+	BMP bmp4("img_test_24bits.bmp");
+	bmp4.write("img_test_24bits_2.bmp");
+
+	BMP bmp6(403, 305, false);
+	bmp6.fill_region(0, 0, 50, 50, 0, 0, 255, 0);
+	bmp6.write("img_test_a.bmp");
+
+	BMP bmp7("img_test_a.bmp");
+	bmp7.fill_region(0, 0, 40, 40, 255, 0, 0, 0);
+	bmp7.write("img_test_a_2.bmp");
+
+	BMP bmp8(200, 200, false);
+	bmp8.fill_region(0, 0, 100, 100, 255, 0, 255, 255);
+	bmp8.write("img_test_24bits_3.bmp");
+}
