a771896052b2954d33813cbc29a98a8fcf8bf3c6
Chris Tralie <chris.tralie@gmail.com>
2020-12-12 14:55:11 -0500

    Fixes #3

--- BMP.h
+++ BMP.h
@@ -194,7 +194,7 @@ struct BMP {
     }
 
     void set_pixel(uint32_t x0, uint32_t y0, uint8_t B, uint8_t G, uint8_t R, uint8_t A) {
-        if (x0 > (uint32_t)bmp_info_header.width || y0 > (uint32_t)bmp_info_header.height) {
+        if (x0 >= (uint32_t)bmp_info_header.width || y0 >= (uint32_t)bmp_info_header.height || x0 < 0 || y0 < 0) {
             throw std::runtime_error("The point is outside the image boundaries!");
         }
 
