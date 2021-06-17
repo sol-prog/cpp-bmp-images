14e5534a4c529196d04a94d219a12fc5ed3f03f4
yhetman <juliagetman5@gmail.com>
2021-06-17 13:02:55 +0300

    |add typedefs and solved Warning: #pragma once in main file|

--- BMP.h
+++ BMP.h
@@ -1,22 +1,24 @@
-#pragma once
-#include <fstream>
-#include <vector>
-#include <stdexcept>
-#include <iostream>
+#ifndef BMP_H
+# define BMP_H
+
+# include <fstream>
+# include <vector>
+# include <stdexcept>
+# include <iostream>
 
 #pragma pack(push, 1)
-struct BMPFileHeader {
+typedef struct	BMPFileHeader {
     uint16_t file_type{ 0x4D42 };          // File type always BM which is 0x4D42 (stored as hex uint16_t in little endian)
     uint32_t file_size{ 0 };               // Size of the file (in bytes)
     uint16_t reserved1{ 0 };               // Reserved, always 0
     uint16_t reserved2{ 0 };               // Reserved, always 0
     uint32_t offset_data{ 0 };             // Start position of pixel data (bytes from the beginning of the file)
-};
+}				t_BMPFileHeader;
 
-struct BMPInfoHeader {
+typedef struct	BMPInfoHeader {
     uint32_t size{ 0 };                      // Size of this header (in bytes)
-    int32_t width{ 0 };                      // width of bitmap in pixels
-    int32_t height{ 0 };                     // width of bitmap in pixels
+    uint32_t width{ 0 };                      // width of bitmap in pixels
+    uint32_t height{ 0 };                     // width of bitmap in pixels
                                              //       (if positive, bottom-up, with origin in lower left corner)
                                              //       (if negative, top-down, with origin in upper left corner)
     uint16_t planes{ 1 };                    // No. of planes for the target device, this is always 1
@@ -27,22 +29,23 @@ struct BMPInfoHeader {
     int32_t y_pixels_per_meter{ 0 };
     uint32_t colors_used{ 0 };               // No. color indexes in the color table. Use 0 for the max number of colors allowed by bit_count
     uint32_t colors_important{ 0 };          // No. of colors used for displaying the bitmap. If 0 all colors are required
-};
+}				t_BMPInfoHeader;
 
-struct BMPColorHeader {
+typedef struct	BMPColorHeader {
     uint32_t red_mask{ 0x00ff0000 };         // Bit mask for the red channel
     uint32_t green_mask{ 0x0000ff00 };       // Bit mask for the green channel
     uint32_t blue_mask{ 0x000000ff };        // Bit mask for the blue channel
     uint32_t alpha_mask{ 0xff000000 };       // Bit mask for the alpha channel
     uint32_t color_space_type{ 0x73524742 }; // Default "sRGB" (0x73524742)
     uint32_t unused[16]{ 0 };                // Unused data for sRGB color space
-};
+}				t_BMPColorHeader;
+
 #pragma pack(pop)
 
 struct BMP {
-    BMPFileHeader file_header;
-    BMPInfoHeader bmp_info_header;
-    BMPColorHeader bmp_color_header;
+    t_BMPFileHeader file_header;
+    t_BMPInfoHeader bmp_info_header;
+    t_BMPColorHeader bmp_color_header;
     std::vector<uint8_t> data;
 
     BMP(const char *fname) {
@@ -246,7 +249,7 @@ private:
 
     // Check if the pixel data is stored as BGRA and if the color space type is sRGB
     void check_color_header(BMPColorHeader &bmp_color_header) {
-        BMPColorHeader expected_color_header;
+        t_BMPColorHeader expected_color_header;
         if(expected_color_header.red_mask != bmp_color_header.red_mask ||
             expected_color_header.blue_mask != bmp_color_header.blue_mask ||
             expected_color_header.green_mask != bmp_color_header.green_mask ||
@@ -258,3 +261,5 @@ private:
         }
     }
 };
+
+#endif
